b530f9e2cf68a9b2ed18e1d6e37114703ee00496
Claudia Flores Saviaga <saviaga@hotmail.com>
2018-04-30 22:07:33 -0500

    create label map

--- create_label_map.py
+++ create_label_map.py
@@ -1,17 +1,16 @@
+"""
+This script creates the label map that we will need for the TF configuration files
+"""
 import argparse
 
-# now we create the pbtxt file, there's no writer for this so we have to make one ourselves
 
-
-def save_label_map(label_map_path, data):
+def create_label_map(label_map_path, data):
     data1=[]
     with open (data,'r') as f1:
         for line in f1:
             data1.append(line)
-    print(data1)
     with open(label_map_path, 'w+') as f:
         for i in range(len(data)):
-            print(data)
             line = "item {\nid: " + str(i + 1) + "\nname: '" + data1[i] + "'\n}\n"
             f.write(line)
 
@@ -20,9 +19,10 @@ parser.add_argument('--trainable_classes_path', dest='trainable_classes', requir
 parser.add_argument('--class_description_path', dest='class_description', required=True)
 parser.add_argument('--label_map_save_path', dest='label_map_path', required=True)
 
+
 if __name__ == '__main__':
     args = parser.parse_args()
     trainable_classes_file = args.trainable_classes
     class_description_file = args.class_description
     label_map_path = args.label_map_path
-    save_label_map(label_map_path, trainable_classes_file)
+    create_labeap(label_map_path, trainable_classes_file)
