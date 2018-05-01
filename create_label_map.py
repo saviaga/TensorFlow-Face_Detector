fe2c5d05f3e420d89176a350d4050739d6e3569f
Claudia Flores Saviaga <saviaga@hotmail.com>
2018-04-30 21:57:45 -0500

    create label map

---
+++ create_label_map.py
@@ -0,0 +1,28 @@
+import argparse
+
+# now we create the pbtxt file, there's no writer for this so we have to make one ourselves
+
+
+def save_label_map(label_map_path, data):
+    data1=[]
+    with open (data,'r') as f1:
+        for line in f1:
+            data1.append(line)
+    print(data1)
+    with open(label_map_path, 'w+') as f:
+        for i in range(len(data)):
+            print(data)
+            line = "item {\nid: " + str(i + 1) + "\nname: '" + data1[i] + "'\n}\n"
+            f.write(line)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--trainable_classes_path', dest='trainable_classes', required=True)
+parser.add_argument('--class_description_path', dest='class_description', required=True)
+parser.add_argument('--label_map_save_path', dest='label_map_path', required=True)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    trainable_classes_file = args.trainable_classes
+    class_description_file = args.class_description
+    label_map_path = args.label_map_path
+    save_label_map(label_map_path, trainable_classes_file)
